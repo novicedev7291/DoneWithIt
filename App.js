c4af13caba87eacb74f4fbca88b47adfbf748a6a
kuldeep <kuldeep.yadav@noosyntech.in>
2020-12-05 15:37:25 +0530

    Some refactoring

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { AppLoading } from "expo";
+import { NavigationContainer } from "@react-navigation/native";
+
+import AppNavigator from "./app/navigations/AppNavigator";
+import AuthContext from "./app/auth/context";
+import authStorage from "./app/auth/storage";
+import navigationTheme from "./app/navigations/navigationTheme";
+import Screen from "./app/components/Screen";
+import OfflineNotice from "./app/components/OfflineNotice";
+import AuthNavigator from "./app/navigations/AuthNavigator";
 
 export default function App() {
+  const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
+
+  const restoreToken = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
+  };
+
+  if (!isReady)
+    return (
+      <AppLoading startAsync={restoreToken} onFinish={() => setIsReady(true)} />
+    );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <Screen>
+        <OfflineNotice />
+        <NavigationContainer theme={navigationTheme}>
+          {user ? <AppNavigator /> : <AuthNavigator />}
+        </NavigationContainer>
+      </Screen>
+    </AuthContext.Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
